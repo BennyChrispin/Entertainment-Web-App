e7dd4dde216cd3b6bbee3d250c197c50a3f87490
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-29 13:34:58 +0200

    some change but still bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,13 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.themoviedb.org/3',
   apiKey: 'edd757d64ecf2fa164eec51450b577b9',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVMwLmBj0LEpDrGJHjvQAC8hw7qRR5ef0',
+    authDomain: 'movie-d46dc.firebaseapp.com',
+    projectId: 'movie-d46dc',
+    storageBucket: 'movie-d46dc.appspot.com',
+    messagingSenderId: '515299775367',
+    appId: '1:515299775367:web:cd5b50cd485ca0b969f922',
+    measurementId: 'G-L2PTHQ4PS1',
+  },
 };
